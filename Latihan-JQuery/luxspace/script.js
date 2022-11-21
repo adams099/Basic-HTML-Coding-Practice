766b275182532e840504463de59785f6080250e2
adam099 <adammusyafa04@gmail.com>
2022-11-21 14:17:10 +0700

    latihan jquery

---
+++ Latihan-JQuery/luxspace/script.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){ 
+    // alert text ketika input email
+    $("#form-email").submit(function(e){    
+        e.preventDefault();
+
+        // input email masuk ke text alert
+        const email = $("#input-email", this).val();
+        $("#text-alert").html("terima kasih <b>" + email + "</b>, link sudah dapat dilihat di email anda.");
+
+        // alert text tampil
+        $("#divAlert").removeClass("d-none");
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    });
+
+    // close text alert
+    $("#btnClose").click(function(){
+        $("#divAlert").addClass("d-none");
+    });
+    // end alert text
+
+
+    // ganti gambar conten yang dipilih
+    $("#imgContent img").click(function() {
+        const imgSrc = $(this).attr("src");
+        $("#imgParent").attr("src", imgSrc);
+        $("#imgParent").css("height", "400");
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    })
+    // end ganti gambar
+
+
+    $("#form-checkout").submit(function(e) {
+        e.preventDefault();
+        // hiden form
+        $(this).addClass("d-none");
+        
+        // value
+        const fristName = $("#first-name", this).val();
+        const lastName = $("#last-name", this).val();
+        const name = fristName + " " + lastName;
+        const address = $("#inputAddress", this).val();
+
+        // kirim vllue
+        $("#nama").html("Terima Kasih, " + name);
+        $("#address").html("Paket akan dikirim ke " + address);
+        
+
+        // show msg
+        $("#msg").removeClass("d-none");
+
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+
+    })
+
+});
